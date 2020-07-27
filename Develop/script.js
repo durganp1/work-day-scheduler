7e3951ca0e714a5e205f38fadd125ba8be2c3f98
durganp1 <durganp1@gmail.com>
2020-07-27 16:48:26 -0500

    competed js

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,48 @@
 
-var tasks = [{
-    text: [],
-    time: []
-}];
+$(document).ready(function() {
 
-//var tasksObj = JSON.parse(localStorage.getItem("tasksObj")) || [];
+    $("#container").on("click", "#save-button", function() {
+        var task = $(this).siblings("#textarea").val();
+        var time = $(this).parent(".input-group").attr("id");
 
-var createTasks = function(taskTime, taskText) {
-    //debugger;
-    var taskNumber = 0;
-    for (i=0; i<(tasks.time).length; i++) {
-    var taskTimeEl = $("#task-time").text();
-    var taskTextEl = $("#textarea").val();
-    console.log(taskTimeEl);
-    console.log(taskTextEl);
-    taskNumber++;
-    }
-};
+        localStorage.setItem(time, task);
+        
+    });
 
-createTasks();
+    var scheduleHandler = function() {
+        var currentTime = moment().hours();
+        $(".input-group").each(function() {
+            var hourBlock = parseInt($(this).attr("id"));
+            if (hourBlock < currentTime) {
+                $(this).addClass("past")
+            } else if (hourBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            };
+        });
+    };
+    scheduleHandler();
 
-// $("#container").on("click", "#textarea",function() {
-//     var taskText = $("#textarea")
-//         .val();
-//     var taskDate = $("#task-time")
-//         .text();
-//         console.log(taskText);
-//         console.log(taskDate);
-//         tasks.push({
-//             text: taskText,
-//             date: taskDate
-//         });
-//         saveTasks();
-//       });
+    var refreshTimer = setInterval(scheduleHandler, 60000);
+
+   
+    $("#8 .form-control").val(localStorage.getItem("8"));
+    $("#9 .form-control").val(localStorage.getItem("9"));
+    $("#10 .form-control").val(localStorage.getItem("10"));
+    $("#11 .form-control").val(localStorage.getItem("11"));
+    $("#12 .form-control").val(localStorage.getItem("12"));
+    $("#13 .form-control").val(localStorage.getItem("13"));
+    $("#14 .form-control").val(localStorage.getItem("14"));
+    $("#15 .form-control").val(localStorage.getItem("15"));
+    $("#16 .form-control").val(localStorage.getItem("16"));
+    $("#17 .form-control").val(localStorage.getItem("17"));
 
 
 
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-// var saveTasks = function () {
-//     localStorage.setItem("tasks", JSON.stringify(tasks));
-// };
-
-// var loadTasks = function() {
-//     tasks = JSON.parse(localStorage.getItem("tasksObj"));
-//     if (!tasks) {
-//         tasks = {
-//             toDo: []
-//     };
-// }
-// $.each(tasks, function(list,array))
-
-// loadTasks();
+});
